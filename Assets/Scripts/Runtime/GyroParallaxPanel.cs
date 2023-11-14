c2a9fbbd303ae628a5bdbf303b2b8989cc20d3b8
Koki Ibukuro <koki.ibukuro@gmail.com>
2023-11-14 10:07:07 +0100

    Implement gyro parallax for mobie

--- Assets/Scripts/Runtime/GyroParallaxPanel.cs
+++ Assets/Scripts/Runtime/GyroParallaxPanel.cs
@@ -1,36 +1,64 @@
 namespace AugmentedInstrument
 {
     using UnityEngine;
-    using UnityEngine.EventSystems;
+    using UnityEngine.Assertions;
     using UnityEngine.InputSystem;
+
+    using Gyroscope = UnityEngine.InputSystem.Gyroscope;
 
     /// <summary>
     /// Rotating panel based on gyroscope (mobile), mouse position (PC).
     /// </summary>
-    public class GyroParallaxPanel : UIBehaviour
+    public class GyroParallaxPanel : MonoBehaviour
     {
         [SerializeField]
-        private float horizontalAngle = 30f;
+        private float _horizontalAngleScale = 30f;
 
         [SerializeField]
-        private float verticalAngle = 30f;
+        private float _verticalAngleScale = 30f;
 
-        private void Update()
+        [SerializeField]
+        private float _clampMaxVelocityMagnitude = 2f;
+
+        [SerializeField]
+        [Range(0.01f, 3f)]
+        private float _smoothTime = 0.5f;
+
+        private Vector3 _velocity;
+
+        private void Start()
         {
             if (Application.isMobilePlatform)
             {
-                Vector3 rotation = Input.gyro.attitude.eulerAngles;
-                transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -rotation.z);
+                Assert.IsNotNull(Gyroscope.current, "Gyroscope is not supported on this device.");
+                InputSystem.EnableDevice(Gyroscope.current);
+            }
+        }
+
+        private void Update()
+        {
+            float delta = Time.deltaTime;
+            Vector3 newVelocity = Vector3.ClampMagnitude(GetAngularVelocity(), _clampMaxVelocityMagnitude);
+            _velocity = Vector3.Lerp(_velocity, newVelocity, delta / _smoothTime);
+
+            Vector3 angles = new(
+                _velocity.y * _verticalAngleScale,
+                _velocity.x * _horizontalAngleScale,
+                0);
+            transform.localRotation = Quaternion.Euler(angles);
+        }
+
+        private Vector3 GetAngularVelocity()
+        {
+            if (Application.isMobilePlatform)
+            {
+                Vector3 v = Gyroscope.current.angularVelocity.ReadValue();
+                return new Vector3(v.y, v.x, v.z);
             }
             else
             {
-                Vector2 screenSize = new(Screen.width, Screen.height);
-                Vector2 position = Mouse.current.position.ReadValue() / screenSize;
-                Vector3 angles = new(
-                    Mathf.Lerp(-verticalAngle, verticalAngle, position.y),
-                    Mathf.Lerp(-horizontalAngle, horizontalAngle, 1f - position.x),
-                    0);
-                transform.localRotation = Quaternion.Euler(angles);
+                Vector3 v = Mouse.current.delta.ReadValue();
+                return new Vector3(-v.x, v.y, 0);
             }
         }
     }
