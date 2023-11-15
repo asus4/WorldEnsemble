d78d6a4ce8ba17f97602f230ead75b0ae152b3c9
Koki Ibukuro <koki.ibukuro@gmail.com>
2023-11-15 14:10:59 +0100

    Optimize AR world mesh shader

--- Assets/Shaders/ARWorldMeshInclude.hlsl
+++ Assets/Shaders/ARWorldMeshInclude.hlsl
@@ -18,4 +18,19 @@ void PeriodicLine_float(
     Alpha = smoothstep(k - w, k, edge) * smoothstep(k + w, k, edge);
 }
 
+void ContactPoint_float(
+    float3 PositionWS,
+    float3 NormalWS,
+    float3 PointWS,
+    float3 PointNormalWS,
+    float Length,
+    out float Out)
+{
+    float distance = length(PositionWS - PointWS);
+    float distanceRatio = saturate(1 - (distance / Length));
+    float frensel = saturate(dot(NormalWS, NormalWS));
+
+    Out = distanceRatio * frensel;
+}
+
 #endif // AR_WORLD_MESH_INCLUDE
