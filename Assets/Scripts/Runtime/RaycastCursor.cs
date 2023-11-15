4e8c7ab50b1cb2e4959c5d2505928bd91ca802de
Koki Ibukuro <koki.ibukuro@gmail.com>
2023-11-15 13:22:57 +0100

    Play sound while raycasting

--- Assets/Scripts/Runtime/RaycastCursor.cs
+++ Assets/Scripts/Runtime/RaycastCursor.cs
@@ -7,21 +7,29 @@ namespace AugmentedInstrument
         [SerializeField]
         private Transform _tip;
 
+        [SerializeField]
+        private AudioSource _raycastHitAudio;
+
         private void Awake()
         {
             SetRaycastHitNone();
         }
-
 
         public void SetRaycastHit(in Ray ray, in RaycastHit hit)
         {
             _tip.gameObject.SetActive(true);
             Quaternion rotation = Quaternion.FromToRotation(Vector3.back, hit.normal);
             _tip.SetPositionAndRotation(hit.point, rotation);
+
+            if (!_raycastHitAudio.isPlaying)
+            {
+                _raycastHitAudio.Play();
+            }
         }
 
         public void SetRaycastHitNone()
         {
+            _raycastHitAudio.Stop();
             _tip.gameObject.SetActive(false);
         }
     }
